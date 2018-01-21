9a86f4534a9fa1fa0215df9de367a5fdb2f840fe
Jorge Marques <mrques.jorge@gmail.com>
2018-01-21 16:39:37 +0000

    Algorithms, Simple Array Sum.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
-mod thirty_days {
-    use std::io;
+mod hr {
+    pub mod thirty_days {
+        use std::io;
 
-    pub fn day1() {
-        let mut input_string = String::new();
+        pub fn _day1() {
+            let mut input_string = String::new();
 
-        io::stdin().read_line(&mut input_string)
-            .expect("Could not read from stdin.");
+            io::stdin().read_line(&mut input_string)
+                .expect("Could not read from stdin");
 
-        println!("Hello World");
-        println!("{}", input_string);
+            println!("Hello World");
+            println!("{}", input_string);
+        }
+    }
+
+    pub mod algorithms {
+        use std::io;
+
+        pub fn _simple_array_sum() {
+            let mut elements = String::new();
+
+            io::stdin().read_line(&mut elements).expect("Could not read from stdin");
+
+            let elements = elements
+                .trim()
+                .split(' ')
+                .map(|x| x.parse::<isize>().expect("Could not parse"));
+
+            let sum: isize = elements.sum();
+
+            println!("{}", sum);
+        }
     }
 }
 
 fn main() {
-    thirty_days::day1();
+    hr::algorithms::_simple_array_sum();
 }
